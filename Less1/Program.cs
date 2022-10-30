c5aef38312fc98e80a368537117fa0b564d872ba
Vladislav <vlad163russia@gmail.com>
2022-10-30 16:13:14 +0400

    add 1steps

--- Less1/Program.cs
+++ Less1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA=5;
+int numberB = 7;
+int max = 0;
+int min = 0;
+    if (numberA > numberB)
+    {
+    Console.WriteLine(max = numberA);
+    Console.WriteLine(min = numberB);
+    }
+    else
+    {
+        Console.WriteLine(max = numberB);
+        Console.WriteLine(max = numberA);
+    }
+    System.Console.WriteLine($"Число {numberA} Меньшее, Число {numberB} Большее");
