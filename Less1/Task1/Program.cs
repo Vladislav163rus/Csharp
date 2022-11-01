5d987f94b613e2ec4b8766317070bc830ce60451
Vladislav <vlad163russia@gmail.com>
2022-11-01 17:20:45 +0400

    add task2

--- Less1/Task1/Program.cs
+++ Less1/Task1/Program.cs
@@ -1,11 +1,11 @@
-﻿int numA ;
-int numB ;
+﻿int numA;
+int numB;
 Console.WriteLine("Введите два числа для сравнения ");
-numA=Convert.ToInt32(Console.ReadLine());
-numB=Convert.ToInt32(Console.ReadLine());
-while (numA>numB)
-Console.WriteLine("Первое число больше");
-if (numA<numB)
-Console.WriteLine("Второе число больше");
+numA = Convert.ToInt32(Console.ReadLine());
+numB = Convert.ToInt32(Console.ReadLine());
+while (numA > numB)
+    Console.WriteLine("Первое число больше");
+if (numA < numB)
+    Console.WriteLine("Второе число больше");
 else Console.WriteLine("Числа равны");
 
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 #  Блок 1
 ## Задание 2
-[Блок схема](https://github.com/Vladislav163rus/Csharp/blob/main/Less1/Task1/Diagrma.drawio.png) ; [Код](https://github.com/Vladislav163rus/Csharp/blob/main/Less1/Task1/Program.cs)
+[Блок схема](https://github.com/Vladislav163rus/Csharp/blob/main/Less1/Task1/Task1.drawio.png) ; [Код](https://github.com/Vladislav163rus/Csharp/blob/main/Less1/Task1/Program.cs)
