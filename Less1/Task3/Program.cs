bd21af472da6e22b985ba127f0ffbeffbf929f67
Vladislav <vlad163russia@gmail.com>
2022-11-01 18:48:58 +0400

    Добавил 3 задание

---
+++ Less1/Task3/Program.cs
@@ -0,0 +1,7 @@
+﻿int num;
+Console.WriteLine("Введите чтобы узнать четное оно или нет ");
+num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+    Console.WriteLine("Число четное");
+else Console.WriteLine("Число нечетное");
+
