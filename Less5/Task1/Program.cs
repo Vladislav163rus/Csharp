d04ade1cea1ee3c92119e1ac7abd7771c9d5088a
Vladislav <vlad163russia@gmail.com>
2022-11-06 19:15:16 +0400

    add less5 task2

---
+++ Less5/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+Random random = new Random();
+int size = 5;
+int[] numbers = new int[size];
+Console.WriteLine(size);
+
+for (int i=0; i< size; i++)
+{
+    numbers[i] = random.Next(100, 1000);
+}
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write(numbers[i] + " ");
+}
+int count = 0;
+for (int i =0; i < size; i++)
+{
+    if (numbers[i] % 2 ==0) count++;
+}
+
+Console.WriteLine("колличество четных элементов " + count);
---
+++ Less5/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+Random random = new Random();
+int size = 5;
+int[] numbers = new int[size];
+// Console.WriteLine(size);
+for (int i = 0; i < size; i++)
+{
+    numbers[i] = random.Next(10, 100);
+}
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write(numbers[i] + " ");
+}
+int sum = 0;
+for (int i = 0; i < size; i++)
+{
+
+    if (i % 2 ==1) sum += numbers [i];
+  
+}
+Console.WriteLine("сумма элементов, стоящих на нечётных позициях " + sum);
