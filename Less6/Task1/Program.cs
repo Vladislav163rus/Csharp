ea5000bdac77669f940192724cf03f4a4aa3eb99
Vladislav <vlad163russia@gmail.com>
2022-11-08 22:16:44 +0400

    add less6

---
+++ Less6/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите элементы(через пробел): ");
+int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов больше 0: {count}");
